29bdaf8945987b83b2484bdb817cc47e173fa656
WyattMeade <wyatt.meade@gmail.com>
2014-08-22 01:27:17 -0400

    added video controls

---
+++ dev/js/scripts.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+
+  // Video
+  var video = document.getElementById("video");
+
+  // Buttons
+  var playButton = document.getElementById("play-pause");
+  var muteButton = document.getElementById("mute");
+  var fullScreenButton = document.getElementById("full-screen");
+
+  // Sliders
+  var seekBar = document.getElementById("seek-bar");
+  var volumeBar = document.getElementById("volume-bar");
+
+}

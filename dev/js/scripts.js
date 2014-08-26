3da21a839b3c8ce7086ed924adf4eccc651e58d1
WyattMeade <wyatt.meade@gmail.com>
2014-08-26 00:00:38 -0400

    some changes and failures

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,11 +1,9 @@
 window.onload = function() {
 
   // Video
-  var video = document.getElementById("video");
+  var $video = document.getElementById(".video");
 
-  // Buttons
-  var playButton = document.getElementById("play-pause");
-  var muteButton = document.getElementById("mute");
-  var fullScreenButton = document.getElementById("full-screen");
+  //Play Button
+  
 
 }

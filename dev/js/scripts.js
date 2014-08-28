b2e7c61973bd25b6c181f5deb01cd385926e4e03
WyattMeade <wyatt.meade@gmail.com>
2014-08-28 00:34:26 -0400

    button icons and functions

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,9 +2,12 @@ window.onload = function() {
 
 	var myVideo = document.getElementById("video1"); 
 
-	document.getElementById("playbutton").onclick = function() {playPause()};
-	document.getElementById("enlargebutton").onclick = function() {makeBig()};
+	document.getElementById("play").onclick = function() {playPause()};
+	document.getElementById("expand").onclick = function() {makeBig()};
+	document.getElementById("shrink").onclick = function() {makeSmall()};
+	document.getElementById("normal").onclick = function() {makeNormal()};
 
+	// Play Button
 	function playPause() {
 
 		if (myVideo.paused) {
@@ -18,26 +21,33 @@ window.onload = function() {
 
 			myVideo.pause();
 			$("i").addClass("fa-play").removeClass("fa-pause");
-			
+
 		}
 
 	};
 
+	// Expand Video
 	function makeBig() { 
-	
-	  myVideo.width = 1080;
+
+	  	myVideo.width = 1080;
+	  	myVideo.height = 720;
 	
 	};
 
+	
+	// Shrink Video
 	function makeSmall() { 
 	
-		myVideo.width = 320; 
+		myVideo.width = 320;
+		myVideo.height = 200;
 	
 	}; 
 
+	// Resize Video to Default
 	function makeNormal() { 
 	
-	  myVideo.width = 720; 
+	  myVideo.width = 720;
+	  myVideo.height = 404; 
 
 	}; 
 
